6cb98912ef683cf1b1530448fed61101626a6a81
MacBookPro <macbookpro@2015s-mbp.home>
2020-11-07 12:07:52 -0500

    first commit from local master

---
+++ src/firstPackage/FirstClass.java
@@ -0,0 +1,18 @@
+package firstPackage;
+
+public class FirstClass {
+	
+	public static void main(String args[]) {
+
+        int counter = 1;
+
+        for (int i = 1; i <= 4; i = i + 1) {
+
+            counter++;
+        }
+        System.out.println(counter);
+    	System.out.println("hello there");
+
+    }
+
+}
